4726a1f04bbbcb0ea1162dc3e80c3c42018f8221
Susetya Mulia Pambudi <susetyamulia@gmail.com>
2024-02-24 14:46:53 +0700

    feat(backend): update, delete, and detail

--- backend/controllers/items.js
+++ backend/controllers/items.js
@@ -15,12 +15,35 @@ const getItems = async (req, res) => {
     });
     await client.query("COMMIT");
   } catch (err) {
-    // logger.error("Unexpected error: ", err);
     console.log(err)
     await client.query("ROLLBACK");
     res.status(500).json([]);
   } finally {
-    // logger.info("Releasing database client.");
+    console.log("Releasing database client.");
+    client.release();
+  }
+  return res;
+};
+
+const getItemById = async (req, res) => {
+  const client = await pool.connect();
+  const { id } = req.params;
+  try {
+    await client.query("BEGIN");
+    const text = `SELECT sku,name, description, weight, width, length, height, image, harga, category_id, category.category_name 
+    FROM item INNER JOIN category ON item.category_id = category.id
+    WHERE item.id = $1`
+    const values = [id]
+    const result = await client.query(text, values);
+    res.status(200).json({
+      data: result?.rows ? result?.rows : [] 
+    });
+    await client.query("COMMIT");
+  } catch (err) {
+    console.log(err)
+    await client.query("ROLLBACK");
+    res.status(500).json([]);
+  } finally {
     console.log("Releasing database client.");
     client.release();
   }
@@ -30,11 +53,57 @@ const getItems = async (req, res) => {
 const createItems = async (req, res) => {
   const client = await pool.connect();
   const {id,sku,name, description, weight, width, length, height, image, harga, category_id} = req.body;
-  console.log(req)
   try {
     await client.query("BEGIN");
     const text = 'INSERT INTO item(id,sku,name,description,weight,width,length,height,image,harga,category_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)'
     const values = [id,sku,name, description, weight, width, length, height, image, harga, category_id]
+    const result = await client.query(text, values);
+    res.status(200).json(result.rows[0]);
+    await client.query("COMMIT");
+  } catch (err) {
+    console.log(err)
+    await client.query("ROLLBACK");
+    res.status(500).json([]);
+  } finally {
+    console.log("Releasing database client.");
+    client.release();
+  }
+  return res;
+};
+
+const updateItems = async (req, res) => {
+  const client = await pool.connect();
+  const { id } = req.params;
+  const { sku,name, description, weight, width, length, height, image, harga, category_id} = req.body;
+  try {
+    await client.query("BEGIN");
+    const text = `UPDATE item SET sku = $1, 
+    name = $2, description = $3,
+    weight = $4,width = $5, length = $6, 
+    height = $7, image = $8,
+    harga = $9 WHERE id = $10`
+    const values = [sku,name, description, weight, width, length, height, image, harga, id]
+    const result = await client.query(text, values);
+    res.status(200).json(result.rows[0]);
+    await client.query("COMMIT");
+  } catch (err) {
+    console.log(err)
+    await client.query("ROLLBACK");
+    res.status(500).json([]);
+  } finally {
+    console.log("Releasing database client.");
+    client.release();
+  }
+  return res;
+};
+
+const deleteItems = async (req, res) => {
+  const client = await pool.connect();
+  const { id } = req.params;
+  try {
+    await client.query("BEGIN");
+    const text = `DELETE FROM item WHERE id = $1`
+    const values = [id]
     const result = await client.query(text, values);
     res.status(200).json(result.rows[0]);
     await client.query("COMMIT");
@@ -55,4 +124,4 @@ const createItems = async (req, res) => {
 
 
 
-module.exports =  { getItems, createItems }
+module.exports =  { getItems, createItems, updateItems, getItemById, deleteItems }
--- backend/routes/item.js
+++ backend/routes/item.js
@@ -1,10 +1,11 @@
-const { getItems, createItems } = require("../controllers/items");
+const { getItems, createItems, updateItems, getItemById, deleteItems } = require("../controllers/items");
 const express = require("express");
 const app = express();
-const itemRouter = express.Router();
 
 app.get("/items", getItems);
+app.get("/items/:id", getItemById);
 app.post("/items",createItems);
-
+app.put("/items/:id", updateItems);
+app.delete("/items/:id", deleteItems);
 
 module.exports = app;
